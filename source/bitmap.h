0a6e5c8aae85f1100719ee24dd2544b1ce3e50d6
Daivuk <daivuk@gmail.com>
2015-08-22 16:12:45 -0400

    Icon composition

--- source/bitmap.h
+++ source/bitmap.h
@@ -12,6 +12,18 @@
 // r1 = Destination bitmap
 .globl bitmap_copy
 
+// Do a plain copy from a bitmap to another one, but alpha blend
+// Destination bitmap must be of same dimensions are source
+// r0 = Source bitmap
+// r1 = Destination bitmap
+.globl bitmap_copyAlpha
+
+// Do a plain copy from a bitmap to another one, but alpha blend additive
+// Destination bitmap must be of same dimensions are source
+// r0 = Source bitmap
+// r1 = Destination bitmap
+.globl bitmap_copyAlphaAdd
+
 // Resize source bitmap to fit into destination bitmap.
 // Both bitmap must match the same bit depth
 // r0 = Source bitmap
@@ -24,6 +36,13 @@
 // r2 = Source bitmap
 // r3 = destination bitmap
 .globl bitmap_blitFull
+
+// Put the full source into destination, but alpha blend
+// r0 = destination X
+// r1 = destination Y
+// r2 = Source bitmap
+// r3 = destination bitmap
+.globl bitmap_blitFullAlpha
 
 // Put the full source with transparency into destination
 // r0 = destination X
@@ -43,6 +62,17 @@
 // r7 = destination bitmap
 .globl bitmap_blit
 
+// Blit a rectangle from a source bitmap to destination bitmap, but alpha blend
+// r0 = source X
+// r1 = source Y
+// r2 = destination X
+// r3 = destination Y
+// r4 = width
+// r5 = height
+// r6 = source bitmap
+// r7 = destination bitmap
+.globl bitmap_blitAlpha
+
 // Blit a rectangle from a source transparent bitmap to destination bitmap
 // r0 = source X
 // r1 = source Y
@@ -54,6 +84,17 @@
 // r7 = destination bitmap
 .globl bitmap_blitTransparent
 
+// Multiply the bitmap with a color
 // r0 = bitmap
 // r1 = color
 .globl bitmap_multColor
+
+// Fill the bitmap with a color
+// r0 = bitmap
+// r1 = color
+.globl bitmap_fill
+
+// Mask out a bitmap using the alpha of another
+// r0 = source bitmap (The mask)
+// r1 = destination bitmap
+.globl bitmap_mask
--- source/ui.h
+++ source/ui.h
@@ -7,3 +7,9 @@
 // r1 = Y position
 // r2 = type
 .globl ui_drawCursor
+
+// Build an Oak Nut icon
+// r0 = destination bitmap
+// r1 = icon color
+// r2 = icons image bitmap
+.globl ui_buildIcon
