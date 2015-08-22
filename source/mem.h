0bfa721f2ca3275313fe730c8c61dac263c119c9
Daivuk <daivuk@gmail.com>
2015-08-22 10:01:37 -0400

    Voodoo

--- source/mem.h
+++ source/mem.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #define sizeof_MemBlock 0x10
-    #define MemBlock_used   0x0 // 4 bytes
-    #define MemBlock_size   0x4 // 4 bytes
-    #define MemBlock_pPrev  0x8 // 4 bytes
-    #define MemBlock_pNext  0xC // 4 bytes
+    #define MemBlock_processId  0x0 // 4 bytes
+    #define MemBlock_size       0x4 // 4 bytes
+    #define MemBlock_pPrev      0x8 // 4 bytes
+    #define MemBlock_pNext      0xC // 4 bytes
 
 .globl mem_getTotalSize
 .globl mem_init
 .globl mem_drawMemUsage
+.globl mem_alloc
+.globl mem_free
