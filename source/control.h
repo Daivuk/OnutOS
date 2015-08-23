2446c2662562b82f717a8b73baf573bea0c4b66d
Daivuk <daivuk@gmail.com>
2015-08-23 10:54:43 -0400

    Starting mouseMove events

--- source/control.h
+++ source/control.h
@@ -2,7 +2,7 @@
 
 #include "object.h"
 
-#define sizeof_Control 0x28
+#define sizeof_Control 0x3C
 // Members
 #define Control_x 0x04 // 4 bytes
 #define Control_y 0x08 // 4 bytes
@@ -14,3 +14,8 @@
 // Callbacks
 #define Control_OnClick 0x20 // 4 bytes
 #define Control_OnPaint 0x24 // 4 bytes
+#define Control_OnMouseEnter 0x28 // 4 bytes
+#define Control_OnMouseLeave 0x2C // 4 bytes
+#define Control_OnMouseDown 0x30 // 4 bytes
+#define Control_OnMouseUp 0x34 // 4 bytes
+#define Control_OnMouseMove 0x38 // 4 bytes
